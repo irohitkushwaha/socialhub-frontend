de90cf85132cf4e6f040212bfb2a4d4f20015092
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-05-21 16:26:52 +0530

    changed to svg of save icon in post

--- src/components/Instagram/Components/Post/Components/PostAction.jsx
+++ src/components/Instagram/Components/Post/Components/PostAction.jsx
@@ -132,7 +132,7 @@ const PostAction = ({ InitialIsLiked, InitialIsSaved, handleLike, postId }) => {
               color={iconColor}
             />
           </button>
-        </div>        
+        </div>
       </div>
       <div>
         {/* Save button */}
@@ -156,12 +156,15 @@ const PostAction = ({ InitialIsLiked, InitialIsSaved, handleLike, postId }) => {
                 color={iconColor}
               />
             ) : (
-              <span
-                className="material-icons"
-                style={{ color: iconColor, fontSize: "36px" }}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="36px"
+                viewBox="0 -960 960 960"
+                width="36px"
+                fill={iconColor}
               >
-                bookmark_border
-              </span>
+                <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z" />
+              </svg>
             )}
           </button>
           {showPromptforSave && (
